cb1f35992ce1fd35c0ab52c924ced51225d07b0b
430lyj <430lyj@gmail.com>
2022-04-15 18:48:26 +0900

    Test: AuthenticationFilter test

--- auth/src/main/java/dev/aquashdw/auth/config/SecurityConfig.java
+++ auth/src/main/java/dev/aquashdw/auth/config/SecurityConfig.java
@@ -1,56 +1,36 @@
 package dev.aquashdw.auth.config;
 
-import dev.aquashdw.auth.config.jwt.JwtAccessDeniedHandler;
-import dev.aquashdw.auth.config.jwt.JwtAuthenticationEntryPoint;
-import dev.aquashdw.auth.config.jwt.JwtProvider;
-import dev.aquashdw.auth.config.jwt.JwtSecurityConfig;
+import dev.aquashdw.auth.filter.MyFilter1;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
+@Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final JwtProvider jwtProvider;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private CorsConfig corsConfig;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler)
-
+    protected void configure(HttpSecurity http) throws Exception{
+        http.addFilterBefore(new MyFilter1(), BasicAuthenticationFilter.class);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 x -> JWT 사용
                 .and()
-                .headers()
-                .frameOptions()
-                .sameOrigin()
-
-                // 시큐리티는 기본적으로 세션을 사용
-                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
-                .and()
+                .addFilter(corsConfig.corsFilter()) // 모든 요청은 이 필터를 탄다, @CrossOrigin(인증 X), 시큐리티 필터에 인증
+                .formLogin().disable() // form tag 만들어서 로그인 안 한다
+                .httpBasic().disable() // http basic 방식 : header에 Authorization 에 Id와 pw를 담아 보냄. -> 우리가 사용하는 건 토큰을 담아 보내는 bearer 방식
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated() // 나머지 API 는 전부 인증 필요
-
-                // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
-                .and()
-                .apply(new JwtSecurityConfig(jwtProvider));
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER')")
+                .anyRequest().permitAll();
     }
 }
